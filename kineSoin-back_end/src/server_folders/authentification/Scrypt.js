e08b4e062609ac6aae9b374b9dc653d0f45b1538
Coada Emilian <emilian.coada@gmail.com>
2025-01-24 14:42:57 +0100

    revisited authentification folder

--- kineSoin-back_end/src/server_folders/authentification/Scrypt.js
+++ kineSoin-back_end/src/server_folders/authentification/Scrypt.js
@@ -1,18 +1,4 @@
-/**
- * @fileoverview This module provides functionality for hashing and comparing passwords
- * using the scrypt algorithm.
- *
- * It includes methods to hash a password securely by generating a random salt and using the
- * scrypt algorithm to derive a secure hash. Additionally, it provides a method to compare
- * a plain text password with a hashed password to verify user credentials.
- *
- * Usage:
- * - Use the `Scrypt.hash` method to hash passwords before storing them.
- * - Use the `Scrypt.compare` method to verify user credentials during authentication.
- *
- * @module Scrypt
- * @requires node:crypto
- */
+// Purpose: Hashes a password using scrypt algorithm from node:crypto.
 
 import { scryptSync, timingSafeEqual, randomBytes, scrypt } from 'node:crypto';
 
