13f238d919e5953aecf44d3e96046dc5ccbb427a
Coada Emilian <emilian.coada@gmail.com>
2025-02-20 15:44:57 +0100

    feat: add fetchPatientDataAsTherapist in apiUtils

--- kineSoin-front_end/src/utils/apiUtils.ts
+++ kineSoin-front_end/src/utils/apiUtils.ts
@@ -1010,3 +1010,18 @@ export const togglePatientStatusAsTherapist = async (id: number) => {
     return false;
   }
 };
+
+export const fetchPatientDataAsTherapist = async (id: number) => {
+  try {
+    const response = await axios.get(`/therapist/me/patients/${id}`);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.error('Failed to fetch patient data', response.data);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching patient data:', error);
+    return null;
+  }
+};
