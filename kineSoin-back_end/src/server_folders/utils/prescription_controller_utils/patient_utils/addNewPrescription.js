e53b311062a3e6644bfeed41258a9d75025354be
Coada Emilian <emilian.coada@gmail.com>
2025-05-20 18:21:20 +0200

    refactor: refactored prescriptionController

---
+++ kineSoin-back_end/src/server_folders/utils/prescription_controller_utils/patient_utils/addNewPrescription.js
@@ -0,0 +1,86 @@
+import Joi from 'joi';
+import { Patient, Prescription } from '../../../models/index.js';
+import { checkIsValidNumber } from '../../checkIsValidNumber.js';
+import { checkPatientStatus } from '../../checkPatientStatus.js';
+
+export default async function addNewPrescription(req, res) {
+  const patient_id = parseInt(req.patient_id, 10);
+
+  checkIsValidNumber(patient_id);
+
+  if (!patient_id) {
+    return res.status(400).json({ message: 'Patient not found' });
+  } else {
+    try {
+      const foundPatient = await Patient.findByPk(patient_id);
+
+      checkPatientStatus(foundPatient);
+
+      if (!req.body) {
+        return res
+          .status(400)
+          .json({ message: 'Please provide the prescription details' });
+      } else {
+        const appointmentQuantity = parseInt(req.body.appointment_quantity, 10);
+
+        if (isNaN(appointmentQuantity)) {
+          req.body.appointment_quantity = null;
+        }
+
+        const prescriptionSchema = Joi.object({
+          medic_id: Joi.number().integer().required(),
+          affliction_id: Joi.number().integer().required(),
+          appointment_quantity: Joi.number().integer().optional().allow(null),
+          at_home_care: Joi.boolean().required(),
+          date: Joi.date().required(),
+        });
+
+        const { error } = prescriptionSchema.validate(req.body);
+
+        if (error) {
+          return res.status(400).json({ message: error.message });
+        } else {
+          const {
+            medic_id,
+            affliction_id,
+            appointment_quantity,
+            at_home_care,
+            date,
+          } = req.body;
+
+          if (!req.file) {
+            return res
+              .status(400)
+              .json({ message: 'Please upload a prescription scan' });
+          }
+
+          const { path, filename } = req.file;
+
+          const picture_url = path;
+          const picture_id = filename;
+
+          const uploadedPrescription = await Prescription.create({
+            medic_id,
+            patient_id: patientId,
+            affliction_id,
+            appointment_quantity,
+            is_new_prescription: true,
+            is_completed: false,
+            at_home_care,
+            date,
+            picture_url,
+            picture_id,
+          });
+
+          res.status(201).json({
+            message: 'Prescription added successfully!',
+            uploadedPrescription,
+          });
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+}
---
+++ kineSoin-back_end/src/server_folders/utils/prescription_controller_utils/patient_utils/getAllPrescriptionsAsPatient.js
@@ -0,0 +1,60 @@
+import { Patient } from '../../../models/index.js';
+import { checkIsValidNumber } from '../../checkIsValidNumber.js';
+import { checkPatientStatus } from '../../checkPatientStatus.js';
+
+export default async function getAllPrescriptionsAsPatient(req, res) {
+  const patient_id = parseInt(req.patient_id, 10);
+
+  checkIsValidNumber(patient_id);
+
+  if (!patient_id) {
+    return res.status(400).json({ message: 'Patient not found' });
+  } else {
+    try {
+      const foundPatient = await Patient.findByPk(patient_id, {
+        attributes: ['id', 'name', 'surname', 'status'],
+        include: [
+          {
+            association: 'prescriptions',
+            required: false,
+            where: { is_completed: false },
+            attributes: [
+              'id',
+              'date',
+              'appointment_quantity',
+              'at_home_care',
+              'picture_url',
+            ],
+            include: [
+              {
+                association: 'medic',
+                attributes: ['id', 'name', 'surname', 'licence_code'],
+              },
+              {
+                association: 'affliction',
+                attributes: [
+                  'id',
+                  'name',
+                  'insurance_code',
+                  'description',
+                  'insurance_code',
+                ],
+              },
+            ],
+          },
+        ],
+      });
+
+      checkPatientStatus(foundPatient);
+
+      if (foundPatient.prescriptions.length === 0) {
+        return res.status(200).json({ prescriptions: [] });
+      } else {
+        res.status(200).json(foundPatient.prescriptions);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+}
