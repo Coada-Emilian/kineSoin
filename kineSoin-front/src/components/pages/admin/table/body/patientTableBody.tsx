d8cbbdf594a310f9c2ba2cce819c3dd4bdd1d6e1
Coada Emilian <emilian.coada@gmail.com>
2026-06-20 14:54:45 +0200

    refactor: refactored patientTableBody and modified adminStatusButton component a bit

--- kineSoin-front/src/components/pages/admin/table/body/patientTableBody.tsx
+++ kineSoin-front/src/components/pages/admin/table/body/patientTableBody.tsx
@@ -1,12 +1,10 @@
-import { Button } from '@headlessui/react';
-import { Link } from 'react-router-dom';
 import type { IPatient } from '../../../../../@types/interfaces/modelInterfaces';
 import type { PatientTableBodyProps } from '../../../../../@types/props/customProps';
 import { useAdminContext } from '../../../../../contexts/AdminContext/useAdminContext';
-import { getPatientTableBodyStatusText } from '../../../../../utils/functions/admin/adminTable/getPatientBodyStatusText';
-import { getPatientTableBodyStatusBackgroundColor } from '../../../../../utils/functions/admin/adminTable/getPatientTableBodyStatusBackgroundColor';
-import deleteIcon from '/icons/delete.png';
-import editIcon from '/icons/edit.png';
+import AdminDeleteButton from '../ui/AdminDeleteButton';
+import AdminEditButton from '../ui/AdminEditButton';
+import AdminStatusButton from '../ui/AdminStatusButton';
+import TableCell from '../ui/TableCell';
 
 export default function PatientTableBody({
   renderedPatients,
@@ -14,73 +12,26 @@ export default function PatientTableBody({
   // Get the openDeleteModal function from the context
   const { openDeleteModal } = useAdminContext();
 
-  const handlePatientDeleteClick = (patient: IPatient) => {
-    openDeleteModal(patient);
-  };
-
-  return renderedPatients.map((patient: IPatient, index: number) => {
-    // Check if the current row is the last row
-    const isLastRow = index === renderedPatients.length - 1;
-
+  return renderedPatients.map((patient: IPatient) => {
     return (
       <tr key={patient.id} className="odd:bg-white even:bg-gray-50">
-        <td
-          className={`border border-gray-300 p-2 text-center ${
-            isLastRow ? 'rounded-bl-2xl' : ''
-          }`}
-        >
-          {patient.id}
+        <TableCell> {patient.id}</TableCell>
+
+        <TableCell>{patient.fullName}</TableCell>
+
+        <td className="border border-gray-300 text-center p-0">
+          <AdminStatusButton status={patient.status}></AdminStatusButton>
         </td>
 
-        <td className="border border-gray-300 px-4 py-2 text-center">
-          {patient.fullName}
-        </td>
+        <TableCell>
+          <AdminEditButton
+            link={`/admin/patients/${patient.id}`}
+          ></AdminEditButton>
+        </TableCell>
 
-        <td
-          className={`border border-gray-300 ${getPatientTableBodyStatusBackgroundColor(
-            patient.status
-          )} px-4 py-2 text-center flex gap-1 items-center font-medium justify-center`}
-        >
-          <p>{`${getPatientTableBodyStatusText(patient.status)}`}</p>
-        </td>
-
-        <td className="border border-gray-300 px-4 py-2 text-center">
-          <Link
-            to={`/admin/patients/${patient.id}`}
-            className="block md:hidden"
-          >
-            <img src={editIcon} alt="edit" className="mx-auto w-10" />
-          </Link>
-
-          <Link
-            to={`/admin/patients/${patient.id}`}
-            className="items-center justify-center hidden md:flex hover:scale-110"
-          >
-            <img src={editIcon} alt="edit" className="w-5 h-5 mx-1" />{' '}
-            <p className="text-blue-300 font-semibold">Inspecter</p>
-          </Link>
-        </td>
-
-        <td
-          className={`border border-gray-300 p-2 text-center ${
-            isLastRow ? 'rounded-br-2xl' : ''
-          }`}
-        >
-          <Button
-            className="mx-auto block md:hidden"
-            onClick={() => handlePatientDeleteClick(patient)}
-          >
-            <img src={deleteIcon} alt="delete" className="w-5 mx-1" />
-          </Button>
-
-          <Button
-            className="w-25 mx-auto items-center hidden md:flex hover:scale-110"
-            onClick={() => handlePatientDeleteClick(patient)}
-          >
-            <img src={deleteIcon} alt="supprimer" className="w-5 mx-1" />
-            <p className="text-red-600 font-semibold">Supprimer</p>
-          </Button>
-        </td>
+        <TableCell>
+          <AdminDeleteButton onDelete={() => openDeleteModal(patient)} />
+        </TableCell>
       </tr>
     );
   });
--- kineSoin-front/src/components/pages/admin/table/ui/AdminStatusButton.tsx
+++ kineSoin-front/src/components/pages/admin/table/ui/AdminStatusButton.tsx
@@ -1,32 +1,37 @@
 import { Button } from '@headlessui/react';
+import { adminTableStatusButtonDetails } from '../../../../../utils/constants/admin/adminTableStatusButtonDetails';
 import refreshIcon from '/icons/refresh.png';
 
 interface Props {
   status: string;
-  onStatusChange: () => void;
+  onStatusChange?: () => void;
 }
 
 export default function AdminStatusButton({ status, onStatusChange }: Props) {
-  const isActive = status === 'active';
+  const buttonData = adminTableStatusButtonDetails.find(
+    (button) => button.status === status
+  );
 
   return (
     <div
       className={`flex items-center justify-center gap-1 font-medium px-4 py-3 ${
-        isActive ? 'bg-green-300' : 'bg-gray-200'
+        buttonData?.background
       }`}
     >
-      <Button
-        onClick={onStatusChange}
-        className="hidden md:block cursor-pointer"
-      >
-        <img
-          src={refreshIcon}
-          alt="change status"
-          className="w-5 hover:animate-spin"
-        />
-      </Button>
+      {onStatusChange && (
+        <Button
+          onClick={onStatusChange}
+          className="hidden md:block cursor-pointer"
+        >
+          <img
+            src={refreshIcon}
+            alt="change status"
+            className="w-5 hover:animate-spin"
+          />
+        </Button>
+      )}
 
-      <p>{isActive ? 'ACTIF' : 'INACTIF'}</p>
+      <p>{`${buttonData?.text}`}</p>
     </div>
   );
 }
