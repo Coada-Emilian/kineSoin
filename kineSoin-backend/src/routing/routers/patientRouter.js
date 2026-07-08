38b4e40cf2588415716f5925a220f6b5ccc563df
Coada Emilian <emilian.coada@gmail.com>
2026-07-08 14:59:32 +0200

    refactor: centralize authentication middleware usage in protected routers

--- kineSoin-backend/src/routing/routers/patientRouter.js
+++ kineSoin-backend/src/routing/routers/patientRouter.js
@@ -21,26 +21,36 @@ import {
   patientPhotoStorage,
   prescriptionScanStorage,
 } from '../../cloudinary/index.js';
+import authenticateUser from '../../middlewares/authenticateUser.js';
 import { controllerWrapper as wrapper } from '../../middlewares/controllerWrapper.js';
-import { authenticatePatient } from '../../middlewares/userAuthentication.js';
-import { afflictionController, appointmentController, authenticationController, insuranceController, medicController, messageController, patientController, prescriptionController, therapistController } from "../controllers/index.js";
+import {
+  afflictionController,
+  appointmentController,
+  authenticationController,
+  insuranceController,
+  medicController,
+  messageController,
+  patientController,
+  prescriptionController,
+  therapistController,
+} from '../controllers/index.js';
 
 const uploadPatientPhoto = multer({ storage: patientPhotoStorage });
 const uploadPrescriptionScan = multer({ storage: prescriptionScanStorage });
 
 export const patientRouter = Router();
 
+patientRouter.use(authenticateUser);
+
 // Route to get all appointments for a patient
 patientRouter.get(
   '/me/appointments',
-  authenticatePatient,
   wrapper(appointmentController.getAllAppointmentsAsPatient)
 );
 
 // Route to create a new prescription for a patient
 patientRouter.post(
   '/me/prescriptions',
-  authenticatePatient,
   uploadPrescriptionScan.single('scan'),
   wrapper(prescriptionController.addNewPrescription)
 );
@@ -48,97 +58,77 @@ patientRouter.post(
 // Route to get all prescriptions for a patient
 patientRouter.get(
   '/me/prescriptions',
-  authenticatePatient,
   wrapper(prescriptionController.getAllPrescriptionsAsPatient)
 );
 
 // Route to get all appointments for a prescription
 patientRouter.get(
   '/me/prescriptions/:prescription_id/appointments',
-  authenticatePatient,
   wrapper(appointmentController.getAllAppointmentsForPrescriptionAsPatient)
 );
 
 // Route to get all messages for a patient
 patientRouter.get(
   '/me/messages',
-  authenticatePatient,
   wrapper(messageController.getAllMessagesAsPatient)
 );
 
 // Route to send a message to a therapist
 patientRouter.post(
   '/me/messages',
-  authenticatePatient,
   wrapper(messageController.sendMessageToTherapist)
 );
 
 // Route to get the personal therapist of a patient
 patientRouter.get(
   '/me/therapist',
-  authenticatePatient,
   wrapper(therapistController.getPersonalTherapistAsPatient)
 );
 
 // Route to get the connected patient data
-patientRouter.get(
-  '/me',
-  authenticatePatient,
-  wrapper(patientController.getConnectedPatientData)
-);
+patientRouter.get('/me', wrapper(patientController.getConnectedPatientData));
 
 // Route to get the insurances of a patient
 patientRouter.get(
   '/me/insurances',
-  authenticatePatient,
   wrapper(insuranceController.getInsurancesAsPatient)
 );
 
 // Route to check the credentials of a patient
 patientRouter.post(
   '/me/checkCredentials',
-  authenticatePatient,
   wrapper(authenticationController.checkPatientPassword)
 );
 
 // Route to add an insurance to a patient
 patientRouter.post(
   '/me/insurance',
-  authenticatePatient,
   wrapper(insuranceController.addInsuranceAsPatient)
 );
 
 // Route to update the connected patient data
-patientRouter.patch(
-  '/me',
-  authenticatePatient,
-  wrapper(patientController.updateConnectedPatient)
-);
+patientRouter.patch('/me', wrapper(patientController.updateConnectedPatient));
 
 // Route to update the insurance of a patient
 patientRouter.patch(
   '/me/insurance',
-  authenticatePatient,
   wrapper(insuranceController.updateInsuranceAsPatient)
 );
 
 // Route to post a photo of a patient
 patientRouter.post(
   '/me/uploadPhoto',
-  authenticatePatient,
   uploadPatientPhoto.single('photo'),
   patientController.uploadPatientPhoto
 );
 
 patientRouter.get(
   '/me/medicNames',
-  authenticatePatient,
   wrapper(medicController.getMedicNamesAsPatient)
 );
 
 patientRouter.get(
   '/me/afflictionNames',
-  authenticatePatient,
   wrapper(afflictionController.getAfflictionNamesAsPatient)
 );
 
